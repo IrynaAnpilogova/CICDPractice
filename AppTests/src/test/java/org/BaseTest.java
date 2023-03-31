9e8986a4e1160389c9f9655fbdf0a43d778e3f8a
Iryna Anpilogova <io.lukash@gmail.com>
2023-03-30 22:59:25 -0700

    Run tests in headless mode

--- AppTests/src/test/java/org/BaseTest.java
+++ AppTests/src/test/java/org/BaseTest.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import java.net.URL;
+// import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class BaseTest {
 
@@ -26,8 +29,16 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
+        // Set any desired options for the Chrome browser
+        // For example, to run Chrome in headless mode:
+        options.addArguments("--headless");
+
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
+
+//        // Create a new instance of the RemoteWebDriver
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+
 //        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
 
